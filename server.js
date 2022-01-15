a54721669aa29b659c1e4f8d55376c41aba36206
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-15 11:37:18 -0700

    mysql database connected to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001; 
@@ -5,6 +6,19 @@ const app = express();
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQl password
+        password: 'mysql123456',
+        database: 'election'
+    },
+    console.log('Connected to the election database!')
+);
 
 
 
